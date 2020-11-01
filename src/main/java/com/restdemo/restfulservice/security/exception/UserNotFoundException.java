e102a1bbedd788b09bae8ac87451b2087d8c04c4
Aditya Mahaddalkar <adityam1311@gmail.com>
2020-11-01 19:01:12 +0530

    Custom exception for authentication purpose

---
+++ src/main/java/com/restdemo/restfulservice/security/exception/UserNotFoundException.java
@@ -0,0 +1,7 @@
+package com.restdemo.restfulservice.security.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String username) {
+        super("User not found with username " + username);
+    }
+}
